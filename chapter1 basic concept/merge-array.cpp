8f2f44f4984356b7bca55cd52a1c3f31eca6a335
Ctum <shuerhy@163.com>
2018-07-01 16:08:40 +0800

    merge two sorted array

---
+++ chapter1 basic concept/merge-array.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void Merge(int A[], int p, int q, int t);
+
+void main()
+{
+    int A[] = {-1, 4, 5, 7, 8, 9, 110,
+                0, 3, 7, 11, 13, 14, 100, 101};
+    const int N = sizeof(A)/sizeof(int);
+}
